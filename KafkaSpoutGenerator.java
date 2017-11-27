967a47f7c2de4be361eaa34973e89dd6c418705d
tongxin.tx <tongxin.tx@alibaba-inc.com>
2017-11-27 17:25:09 +0800

    add group id record translator

--- KafkaSpoutGenerator.java
+++ KafkaSpoutGenerator.java
@@ -1,6 +1,10 @@
 package logparser.spout;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.storm.kafka.spout.KafkaSpoutConfig;
+import org.apache.storm.kafka.spout.ByTopicRecordTranslator;
 import org.apache.storm.kafka.spout.KafkaSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
 
 public class KafkaSpoutGenerator extends KafkaSpout {
     private static final String bootstrapServers = "10.206.28.74:9092,10.206.28.8:9092,10.206.28.81:9092,10.206.28.89:9092,10.206.3.121:9092,10.206.3.127:9092,10.206.32.104:9092,10.206.32.107:9092";
@@ -15,7 +19,10 @@ public class KafkaSpoutGenerator extends KafkaSpout {
     }
 
     public static KafkaSpoutConfig<String, String> getKafkaSpoutConfig() {
+        ByTopicRecordTranslator<String, String> trans = new ByTopicRecordTranslator<>((r) -> new Values(r.value()),new Fields("value"));
         return KafkaSpoutConfig.builder(bootstrapServers, kafkaTopic)
+            .setProp(ConsumerConfig.GROUP_ID_CONFIG, "kafkaSpoutLogParser")
+            .setRecordTranslator(trans)
             .setOffsetCommitPeriodMs(10_000)
             .setFirstPollOffsetStrategy(KafkaSpoutConfig.FirstPollOffsetStrategy.LATEST)
             .setMaxUncommittedOffsets(250)
